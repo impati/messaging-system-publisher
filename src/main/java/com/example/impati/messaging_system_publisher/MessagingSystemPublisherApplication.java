18ded0aa1991f92a924d4fec8fb6bdecc1f06ae1
impati <yongs170@naver.com>
2025-04-19 21:49:43 +0900

    first commit

---
+++ src/main/java/com/example/impati/messaging_system_publisher/MessagingSystemPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.example.impati.messaging_system_publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingSystemPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingSystemPublisherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/impati/messaging_system_publisher/MessagingSystemPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.impati.messaging_system_publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingSystemPublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
